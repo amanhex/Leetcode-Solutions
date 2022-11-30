3f5607ac1c24f42e1b95ecdcb35583984918736b
amanhex <aamankumar4958@gmail.com>
2022-11-30 19:41:55 +0530

    Merge Two Sorted Lists

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -51,3 +51,38 @@ public:
         }
     }
 };
+
+// Iterative Approach
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)
+            return list2;
+        if (list2 == nullptr)
+            return list1;
+        ListNode* result = list1;
+        if (list1 -> val > list2 -> val){
+            result = list2;
+            list2 = list2 -> next;
+        }
+        else
+            list1 = list1 -> next;
+        ListNode* current = result;
+        while (list1 && list2){
+            if (list1 -> val < list2 -> val){
+                current -> next = list1;
+                list1 = list1 -> next;
+            }
+            else{
+                current -> next = list2;
+                list2 = list2 -> next;
+            }
+            current = current -> next;
+        }
+        if (!list1)
+            current -> next = list2;
+        else
+            current -> next = list1;
+        return result;
+    }
+};
