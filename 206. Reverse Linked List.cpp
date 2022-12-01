a2a9d3621fb5f6c3932c9fc97e669cc36d5172fa
amanhex <aamankumar4958@gmail.com>
2022-12-01 20:19:01 +0530

    Reverse Linked List

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,52 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+// Iterative Approach (Single Iteration of Linked List)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head -> next == nullptr)
+            return head;
+        ListNode* temp = head -> next;
+        ListNode* ahead = nullptr, *prev = head;
+        while (temp && temp -> next){
+            ahead = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = ahead;
+        }
+        temp -> next = prev;
+        head -> next = nullptr;
+        return temp;
+    }
+};
