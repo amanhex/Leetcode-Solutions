3ad6a72dbb2a81b84f037de368824f746dd5bcef
amanhex <aamankumar4958@gmail.com>
2022-12-12 22:39:13 +0530

    Maximum Depth of Binary Tree

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -32,6 +32,6 @@ public:
     int maxDepth(TreeNode* root){
         if (!root)
             return 0;
-        return max(maxDepth(root -> left), maxDepth(root -> right)) + 1;
+        return 1 + max(maxDepth(root -> left), maxDepth(root -> right));
     }
 };
