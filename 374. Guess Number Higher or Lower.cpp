d67df0ffdadc79b25dede8883009624c65e86f23
amanhex <aamankumar4958@gmail.com>
2022-12-14 22:40:22 +0530

    Guess Number Higher or Lower

---
+++ 374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,53 @@
+/*
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+
+Constraints:
+1 <= n <= 231 - 1
+1 <= pick <= n
+*/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int low, int high){
+        int mid = low + (high - low) / 2;
+        int ans = guess(mid);
+        if (ans == -1)
+            return guessNumber(low, mid - 1);
+        else if (ans == 1)
+            return guessNumber(mid + 1, high);
+        else
+            return mid;
+    }
+    int guessNumber(int n) {
+        return guessNumber(1, n);
+    }
+};
