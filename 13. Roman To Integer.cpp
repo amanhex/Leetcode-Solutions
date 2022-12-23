a6e77310d155a820dab5adbb1537f869c943592f
amanhex <80842658+amanhex@users.noreply.github.com>
2022-12-23 23:02:18 +0530

    Update 13. Roman To Integer.cpp

--- 13. Roman To Integer.cpp
+++ 13. Roman To Integer.cpp
@@ -23,34 +23,28 @@ X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 
- 
-
 Example 1:
-
 Input: s = "III"
 Output: 3
 Explanation: III = 3.
-Example 2:
 
+Example 2:
 Input: s = "LVIII"
 Output: 58
 Explanation: L = 50, V= 5, III = 3.
-Example 3:
 
+Example 3:
 Input: s = "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
 
 Constraints:
-
 1 <= s.length <= 15
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 */
 
 // using hashmaps
-
 class Solution {
 public:
 int romanToInt(string s) {
