5731d94deef742afd87415f97b58d2c2f05ac35a
amanhex <aamankumar4958@gmail.com>
2022-12-28 22:22:02 +0530

    Add Digits

--- 258. Add Digits.cpp
+++ 258. Add Digits.cpp
@@ -17,8 +17,11 @@ Constraints:
 0 <= num <= 231 - 1
 
 Follow up: Could you do it without any loop/recursion in O(1) runtime?
+(calculating digital roots is O(1) runtime solution)
 */
 
+
+// Recursive Solution 
 class Solution {
 public:
     int addDigits(int num) {
