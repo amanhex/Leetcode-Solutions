b6491ad2c785d5abe3a55627084e3c9d21787dea
amanhex <aamankumar4958@gmail.com>
2022-12-30 23:24:44 +0530

    Palindrome Number

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,7 +1,5 @@
 /*
-Given an integer x, return true if x is a 
-palindrome
-, and false otherwise.
+Given an integer x, return true if x is a palindrome, and false otherwise.
 
 Example 1:
 Input: x = 121
