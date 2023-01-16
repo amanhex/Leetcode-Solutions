f35be329b9205ccba6bfbd59659c15e4daeef2de
amanhex <aamankumar4958@gmail.com>
2023-01-16 22:50:45 +0530

    Reverse String

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -3,20 +3,15 @@ Write a function that reverses a string. The input string is given as an array o
 
 You must do this by modifying the input array in-place with O(1) extra memory.
 
- 
-
 Example 1:
-
 Input: s = ["h","e","l","l","o"]
 Output: ["o","l","l","e","h"]
-Example 2:
 
+Example 2:
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
- 
 
 Constraints:
-
 1 <= s.length <= 105
 s[i] is a printable ascii character.
 */
