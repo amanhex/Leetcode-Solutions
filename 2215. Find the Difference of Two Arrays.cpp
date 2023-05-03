00a7a290357bf675ae509ee53a4b29ea26f49f55
Amanhex <aamankumar4958@gmail.com>
2023-05-03 21:22:03 +0530

    Find the Difference of Two Arrays

--- 2215. Find the Difference of Two Arrays.cpp
+++ 2215. Find the Difference of Two Arrays.cpp
@@ -23,6 +23,27 @@ Constraints:
 -1000 <= nums1[i], nums2[i] <= 1000
 */
 
+// Using unordered_set
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end()), set2(nums2.begin(), nums2.end());
+        vector<int> temp1, temp2;
+        for (int i : set1) {
+            if (set2.count(i) == 0) {
+                temp1.push_back(i);
+            }
+        }
+
+        for (int i : set2) {
+            if (set1.count(i) == 0) {
+                temp2.push_back(i);
+            }
+        }
+        return {temp1, temp2};
+    }
+};
+
 // With using Hashmap
 class Solution {
 public:
