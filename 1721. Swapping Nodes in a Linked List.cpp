6b13b2833f5b92b5c48e4912a3326b882cd8dedc
Amanhex <aamankumar4958@gmail.com>
2023-05-15 22:54:21 +0530

    Swapping Nodes in a Linked List

---
+++ 1721. Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,48 @@
+/*
+You are given the head of a linked list, and an integer k.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+
+Constraints:
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *left = head, *right = head;
+        int i = 0;
+        while (i < k - 1){
+            right = right -> next;
+            i++;
+        }
+        ListNode *end = right;
+        while (right -> next){
+            left = left -> next;
+            right = right -> next;
+        }
+        int temp = left -> val;
+        left -> val = end -> val;
+        end -> val = temp;
+        return head;
+    }
+};
