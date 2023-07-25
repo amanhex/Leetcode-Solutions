6e2265557017594a17b39c4b5de7bf51dfdec575
Amanhex <aamankumar4958@gmail.com>
2023-07-25 13:53:36 +0530

    Search a 2D Matrix

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -33,7 +33,7 @@ public:
     }
 };
 
-// Better Approach
+// Better Approach 1
 
 class Solution {
 public:
@@ -49,6 +49,25 @@ public:
                     else
                         start = mid + 1;
                 }
+        }
+        return false;
+    }
+};
+
+// Better Approach 2 (More Better than previous solutions)
+// Two pointer approach
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int left = 0, right = m - 1;
+        while (left < n && right >= 0){
+            if (matrix[left][right] == target)
+                return true;
+            else if (matrix[left][right] > target)
+                right--;
+            else
+                left++;
         }
         return false;
     }
