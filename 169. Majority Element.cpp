5db3af7180b233694fedd9a134ef41cdc2ccdb50
Amanhex <aamankumar4958@gmail.com>
2023-07-27 13:33:08 +0530

    Majority Element

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -30,3 +30,22 @@ public:
         return 0;
     }
 };
+
+// Optimal Approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element, count = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (count == 0){
+                element = nums[i];
+                count++;
+            }
+            else if (nums[i] == element)
+                count++;
+            else
+                count--;
+        }
+        return element;
+    }
+};
