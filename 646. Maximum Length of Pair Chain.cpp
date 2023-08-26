665a84a1e0a09855d8b4f800406f9a391ea27f7d
Amanhex <aamankumar4958@gmail.com>
2023-08-26 18:05:42 +0530

    Maximum Length of Pair Chain

---
+++ 646. Maximum Length of Pair Chain.cpp
@@ -0,0 +1,44 @@
+/*
+You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+
+Return the length longest chain which can be formed.
+
+You do not need to use up all the given intervals. You can select pairs in any order.
+
+Example 1:
+Input: pairs = [[1,2],[2,3],[3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4].
+
+Example 2:
+Input: pairs = [[1,2],[7,8],[4,5]]
+Output: 3
+Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
+
+Constraints:
+n == pairs.length
+1 <= n <= 1000
+-1000 <= lefti < righti <= 1000
+*/
+
+class Solution {
+public:
+    static bool comparision(vector<int>& a, vector<int>& b){
+        return a[1] < b[1];
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), comparision);
+        int count = 1;
+        int last = pairs[0][1];
+        for (int i = 1; i < pairs.size(); i++){
+            if (pairs[i][0] > last){
+                count++;
+                last = pairs[i][1];
+            }
+        }
+        return count;
+    }
+};
