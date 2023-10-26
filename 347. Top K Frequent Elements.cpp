acbf787129a3473d99bba84d2d23ce0ef0cb7e88
amanhex <aamankumar4958@gmail.com>
2023-10-26 18:18:48 +0530

    Top K Frequent Elements

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,42 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+
+Constraints:
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+*/
+
+// Using priority_queue
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for (auto i : nums)
+            map[i]++;
+        
+        priority_queue<pair<int, int>> pq;
+
+        for (auto it = map.begin(); it != map.end(); it++)
+            pq.push({it -> second, it -> first});
+
+        vector<int> ans;
+        while (k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
