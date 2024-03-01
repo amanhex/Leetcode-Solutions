ac75a9bfb536b1b1e97fece2cc2b3fbd04ee69d9
amanhex <aamankumar4958@gmail.com>
2024-03-01 14:34:25 +0530

    Maximum Odd Binary Number

---
+++ 2864. Maximum Odd Binary Number.cpp
@@ -0,0 +1,51 @@
+/*
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+Example 1:
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+
+Example 2:
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+
+Constraints:
+    1 <= s.length <= 100
+    s consists only of '0' and '1'.
+    s contains at least one '1'.
+*/
+
+// Sorting
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        sort(s.rbegin(), s.rend());
+        for (int i = s.size() - 1; i >= 0; i--){
+            if (s[i] == '1')
+                swap(s[i], s[s.size() - 1]);
+        }
+        return s;
+    }
+};
+
+// Counting
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int cnt1 = 0, cnt0 = 0;
+        for (auto a : s){
+            if (a == '1') cnt1++;
+            else if (a == '0') cnt0++;
+        }
+        string ans = string(cnt1 - 1, '1') + string(cnt0, '0') + '1';
+        return ans;
+    }
+};
