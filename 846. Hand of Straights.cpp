63619f267f20fbd8894ec58a32370e212a5100af
amanhex <aamankumar4958@gmail.com>
2024-06-07 01:14:47 +0530

    Hand of Straights

---
+++ 846. Hand of Straights.cpp
@@ -0,0 +1,48 @@
+/*
+Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+
+Example 1:
+Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+Output: true
+Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+
+Example 2:
+Input: hand = [1,2,3,4,5], groupSize = 4
+Output: false
+Explanation: Alice's hand can not be rearranged into groups of 4.
+
+Constraints:
+    1 <= hand.length <= 104
+    0 <= hand[i] <= 109
+    1 <= groupSize <= hand.length
+*/
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+        unordered_map<int, int> mp;
+        for (int i : hand)
+            mp[i]++;
+
+        vector<int> sortedKeys;
+        for (auto pair : mp)
+            sortedKeys.push_back(pair.first);
+        sort(sortedKeys.begin(), sortedKeys.end());
+        
+        for (int key : sortedKeys){
+            if (mp[key] > 0){
+                int startCount = mp[key];
+                for (int i = key; i < key + groupSize; i++){
+                    if (mp[i] < startCount)
+                        return false;
+                    mp[i] -= startCount;
+                }
+            }
+        }
+
+        return true;
+    }
+};
